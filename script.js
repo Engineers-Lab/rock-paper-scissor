a1a12d0ee78033daed1ce24e51e341d296af769b
Engineers-Lab <ais.maazaz@gmail.com>
2023-02-13 20:32:46 +0100

    implementation of checkMatch function

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper' , 'Scissors']
+    const choices = ['rock', 'raper' , 'scissors']
     let randIndex = Math.floor(Math.random()*choices.length)
     let choice = choices[randIndex]
-    console.log(choice)
+    console.log("computer ", choice)
     return choice
 }
 
-getComputerChoice()
+function getUserSelection() {
+    const choice = prompt('make your choice : rock, paper or scissors').toLowerCase()
+    return choice
+}
+
+function checkMatch() {
+    const userChoice = getUserSelection()
+    const computerChoice = getComputerChoice()
+
+    switch (userChoice) {
+        case 'rock': {
+            if (computerChoice == 'paper') {
+                console.log('you lost to the computer!')
+            } else if (computerChoice == 'scissors') {
+                console.log('you win the computer!')
+            } else {
+                console.log('EVEN')
+            }
+        }
+            break;
+        case 'paper': {
+            if (computerChoice == 'scissors') {
+                console.log('you lost to the computer!')
+            } else if (computerChoice == 'rock') {
+                console.log('you win the computer!')
+            }else {
+                console.log('EVEN')
+            }
+            break;
+        }
+        case 'scissors': {   
+            if (computerChoice == 'rock') {
+                console.log('you lost to the computer!')
+            } else if (computerChoice == 'paper') {
+                console.log('you win the computer!')
+            } else {
+                console.log('EVEN')
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+// getComputerChoice()
+checkMatch()
