6e9e2b4e9cadce46cca6587546d642bd960e6709
Engineers-Lab <ais.maazaz@gmail.com>
2023-02-13 23:28:07 +0100

    made score variables, and result sys

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    const choices = ['rock', 'raper' , 'scissors']
-    let randIndex = Math.floor(Math.random()*choices.length)
+    const choices = ['rock', 'paper', 'scissors']
+    let randIndex = Math.floor(Math.random() * choices.length)
     let choice = choices[randIndex]
     console.log("computer ", choice)
     return choice
@@ -18,31 +18,40 @@ function checkMatch() {
     switch (userChoice) {
         case 'rock': {
             if (computerChoice == 'paper') {
-                console.log('you lost to the computer!')
+                // console.log('you lost to the computer!')
+                computerScore += 1
             } else if (computerChoice == 'scissors') {
-                console.log('you win the computer!')
+                // console.log('you win the computer!')
+                userScore += 1
             } else {
-                console.log('EVEN')
+                // console.log('EVEN rock')
+                break
             }
         }
             break;
         case 'paper': {
             if (computerChoice == 'scissors') {
-                console.log('you lost to the computer!')
+                // console.log('you lost to the computer!')
+                computerScore += 1
             } else if (computerChoice == 'rock') {
-                console.log('you win the computer!')
-            }else {
-                console.log('EVEN')
+                // console.log('you win the computer!')
+                userScore += 1
+            } else {
+                // console.log('EVEN paper')
+                break
             }
             break;
         }
-        case 'scissors': {   
+        case 'scissors': {
             if (computerChoice == 'rock') {
-                console.log('you lost to the computer!')
+                // console.log('you lost to the computer!')
+                computerScore += 1
             } else if (computerChoice == 'paper') {
-                console.log('you win the computer!')
+                // console.log('you win the computer!')
+                userScore += 1
             } else {
-                console.log('EVEN')
+                // console.log('EVEN scissors')
+                break
             }
             break;
         }
@@ -51,4 +60,19 @@ function checkMatch() {
     }
 }
 // getComputerChoice()
-checkMatch()
+let userScore = 0
+let computerScore = 0
+for (let index = 0; index < 5; index++) {
+    checkMatch()
+}
+
+if (userScore > computerScore) {
+    console.log('you win the computer!')
+} else if (userScore < computerScore) {
+    console.log('you lost to the computer!')
+} else {
+    console.log('EVEN scissors')
+}
+
+console.log('userscore', userScore)
+console.log('computerscore', computerScore) 
